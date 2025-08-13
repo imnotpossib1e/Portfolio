6ef9d259025dfdd90a5dec11bfa881ac0b37a316
imnotpossib1e <habit2me@gmail.com>
2025-08-13 14:21:37 +0900

    💄 style: 파비콘, 로고 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { FileUser } from "lucide-react";
+
 interface HeaderProps {
   onTitleClick: () => void;
   onAboutClick: () => void;
@@ -8,8 +10,8 @@ interface HeaderProps {
 export default function Header({ onTitleClick, onAboutClick, onSkillsClick, onProjectsClick }: HeaderProps) {
   return (
     <header className="fixed top-0 p-4 flex justify-between w-full bg-violet-400 z-10">
-      <h1 onClick={onTitleClick} className="md:text-2xl text-xl font-semibold">
-        JinHyun Portfolio
+      <h1 onClick={onTitleClick} className="flex items-center gap-2 md:text-2xl text-xl font-semibold">
+        <FileUser /> JinHyun Portfolio
       </h1>
       <div className="flex gap-6 md:text-xl text-base font-semibold">
         <button onClick={onAboutClick}>About Me</button>
