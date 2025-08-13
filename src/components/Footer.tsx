1a8648cc638aca614dacbfe4393a134e2d9ccf37
imnotpossib1e <habit2me@gmail.com>
2025-08-13 14:25:14 +0900

    💄 style: 푸터 저작권 표시 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,11 @@ import { Copyright } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center h-24">
-      <Copyright width={15} /> 2025. Lee Jin Hyun All rights reserved.
+    <footer className="flex flex-col justify-center items-center h-24">
+      <div className="flex">
+        <Copyright width={15} /> <span>2025. Lee Jin Hyun All rights reserved.</span>
+      </div>
+      <span className="mt-1 text-sm text-gray-500">favicon 아이콘 제작자: deemakdaksina - Flaticon </span>
     </footer>
   );
 }
